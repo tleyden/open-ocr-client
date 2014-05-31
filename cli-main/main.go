0092a8c3953f4dd2f832d8ec726c70a03547682a
Traun Leyden <tleyden@couchbase.com>
2014-05-31 12:55:45 -0700

    cli to run stress test

---
+++ cli-main/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+
+	"os"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/couchbaselabs/logg"
+	"github.com/tleyden/open-ocr-client"
+)
+
+var (
+	app           = kingpin.New("open-ocr-client", "A command-line chat application.")
+	stress        = app.Command("stress", "Do a stress test")
+	numIterations = stress.Arg("numIterations", "how many OCR jobs should it create").Default("100").Int()
+	numTestImages = 5
+)
+
+func init() {
+	logg.LogKeys["CLI"] = true
+}
+
+func main() {
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	case "stress":
+		logg.LogTo("CLI", "do stress test")
+		stressTest()
+	default:
+		logg.LogTo("CLI", "oops, nothing to do")
+	}
+}
+
+func imageUrls() []string {
+	imageUrlBase := "http://tleyden-misc.s3.amazonaws.com/ocr-test-data"
+
+	imageUrls := []string{}
+	for i := 0; i < numTestImages; i++ {
+		imageUrl := fmt.Sprintf("%s/%d.png", imageUrlBase, i)
+		imageUrls = append(imageUrls, imageUrl)
+	}
+	return imageUrls
+}
+
+func stressTest() {
+
+	imageUrls := imageUrls()
+	logg.LogTo("CLI", "imageUrls: %v", imageUrls)
+	logg.LogTo("CLI", "numIterations: %v", *numIterations)
+
+	openOcrUrl := "http://api.openocr.net"
+	client := ocrclient.NewHttpClient(openOcrUrl)
+
+	for i := 0; i < *numIterations; i++ {
+		index := randomIntInRange(0, numTestImages)
+		imageUrl := imageUrls[index]
+		logg.LogTo("CLI", "OCR decoding: %v.  index: %d", imageUrl, index)
+		ocrDecoded, err := client.DecodeImageUrl(imageUrl, ocrclient.ENGINE_TESSERACT)
+		if err != nil {
+			logg.LogPanic("Error decoding image: %v", err)
+		}
+		logg.LogTo("CLI", "OCR decoded: %v", ocrDecoded)
+	}
+
+}
+
+// return a random number between min and max - 1
+// eg, if you call it with 0,1 it will always return 0
+// if you call it between 0,2 it will return 0 or 1
+func randomIntInRange(min, max int) int {
+	if min == max {
+		return min
+	}
+	return rand.Intn(max-min) + min
+}
