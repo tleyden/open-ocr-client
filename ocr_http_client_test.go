590f5c14528f59f7a2d050d8796ae3499a857347
Traun Leyden <tleyden@couchbase.com>
2014-08-23 12:30:11 -0700

    multipart nearly working

--- ocr_http_client_test.go
+++ ocr_http_client_test.go
@@ -1,11 +1,13 @@
 package ocrclient
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
+	"os"
 	"testing"
 
 	"github.com/couchbaselabs/go.assert"
+	"github.com/couchbaselabs/logg"
 	"github.com/tleyden/fakehttp"
 )
 
@@ -31,18 +33,23 @@ func TestDecodeImageReader(t *testing.T) {
 
 	port := 8080
 	fakeDecodedOcr := "fake ocr"
-	sourceServer := fakehttp.NewHTTPServerWithPort(port)
-	sourceServer.Start()
-	headers := map[string]string{"Content-Type": "text/plain"}
-	sourceServer.Response(200, headers, fakeDecodedOcr)
+	/*
 
+
+		sourceServer := fakehttp.NewHTTPServerWithPort(port)
+		sourceServer.Start()
+		headers := map[string]string{"Content-Type": "text/plain"}
+		sourceServer.Response(200, headers, fakeDecodedOcr)
+	*/
 	openOcrUrl := fmt.Sprintf("http://localhost:%d", port)
 	openOcrClient := NewHttpClient(openOcrUrl)
 
-	testImageContents := "blah"
-	reader := strings.NewReader(testImageContents)
+	file, err := os.Open("ocr_test.png")
+	assert.True(t, err == nil)
+	reader := bufio.NewReader(file)
 
 	ocrDecoded, err := openOcrClient.DecodeImageReader(reader, ENGINE_TESSERACT)
+	logg.Log("err: %v", err)
 	assert.True(t, err == nil)
 	assert.Equals(t, ocrDecoded, fakeDecodedOcr)
 
